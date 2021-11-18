83be75cc1ca3f61bbb1c42d396a604efcea50988
DevVad <vadim.rgsu@yandex.ru>
2021-11-18 23:48:58 +0300

    fix images

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ const IndexPage = () => {
       `}
       render={data => {
 				return data.allFile.edges.map((item) => {
-					return <img src={item.node.publicURL} width="200px" key={item.node.id}/>
+					return <img src={item.node.publicURL} width="350px" key={item.node.id}/>
 				})
 			}}
     />
