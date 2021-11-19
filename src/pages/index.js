e86631f83eb034ebc2aa62431f0186cc2709d3f0
DevVad <vadim.rgsu@yandex.ru>
2021-11-19 17:58:44 +0300

    StaticImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,31 +1,9 @@
 import * as React from "react"
-import { StaticQuery, graphql } from "gatsby"
+import { StaticImage, graphql } from "gatsby"
 import * as pageStyles from "./page.module.css"
 
 const IndexPage = () => {
-  return (
-    <StaticQuery
-      query={graphql`
-        query AssetsPhotos {
-          allFile(
-            filter: {extension: {regex: "/jpg$/"}, relativeDirectory: {glob: "*"}}
-          ) {
-            edges {
-              node {
-                publicURL
-                id
-              }
-            }
-          }
-        }
-      `}
-      render={data => {
-        return data.allFile.edges.map((item) => {
-          return <img className={pageStyles.page} src={item.node.publicURL} alt="Dogs" width="430px" key={item.node.id}/>
-        })
-      }}
-    />
-  )
+  return <StaticImage className={pageStyles.page} src={sobaki_tsvety_shlyapy_eda_93411_1920x1080.png} alt="Dogs" width="430px" key={item.node.id}/>
 }
 
 export default IndexPage
