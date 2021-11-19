ae6c07bc197ac047aab02a828cf37732729b898b
DevVad <vadim.rgsu@yandex.ru>
2021-11-19 12:05:40 +0300

    processing project

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,26 +3,26 @@ import { StaticQuery, graphql } from "gatsby"
 
 const IndexPage = () => {
   return (
-		<StaticQuery
+    <StaticQuery
       query={graphql`
-				query AssetsPhotos {
-					allFile(
-						filter: {extension: {regex: "/jpg$/"}, relativeDirectory: {glob: "*"}}
-					) {
-						edges {
-							node {
-								publicURL
-								id
-							}
-						}
-					}
-				}
+        query AssetsPhotos {
+          allFile(
+            filter: {extension: {regex: "/jpg$/"}, relativeDirectory: {glob: "*"}}
+          ) {
+            edges {
+              node {
+                publicURL
+                id
+              }
+            }
+          }
+        }
       `}
       render={data => {
-				return data.allFile.edges.map((item) => {
-					return <img src={item.node.publicURL} width="350px" key={item.node.id}/>
-				})
-			}}
+        return data.allFile.edges.map((item) => {
+          return <img src={item.node.publicURL} width="350px" key={item.node.id}/>
+        })
+      }}
     />
   )
 }
