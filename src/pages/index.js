4b2ea321f5d5b2dbb48fd77ef305610dfa2b292b
DevVad <vadim.rgsu@yandex.ru>
2021-11-21 19:56:48 +0300

    make StaticImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,9 @@
 import * as React from "react"
-import { StaticImage, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 import * as pageStyles from "./page.module.css"
 
 const IndexPage = () => {
-  return <StaticImage className={pageStyles.page} src={sobaki_tsvety_shlyapy_eda_93411_1920x1080.png} alt="Dogs" width="430px" key={item.node.id}/>
+  return <StaticImage className={pageStyles.page} src="../images/sobaki_bezhat_pryzhok_voda_56684_1920x1080.png" alt="Dogs" width="430px"/>
 }
 
 export default IndexPage
